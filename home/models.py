f85a316a96d674c6069a8e3ad9e15a5e145e7d86
oussama zouaki <oussamazouaki4@gmail.com>
2020-03-31 23:13:00 +0100

    add menus models

--- home/models.py
+++ home/models.py
@@ -2,11 +2,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.admin.edit_handlers import InlinePanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import (
+        InlinePanel,
+        MultiFieldPanel,
+        StreamFieldPanel,
+        FieldPanel,
+        PageChooserPanel,
+)
+from wagtail.snippets.models import register_snippet
 
 from home import blocks
 
@@ -53,3 +61,49 @@ class HomePage(Page):
 
     def get_admin_display_title(self):
         return _("Accueil")
+
+class MenuItem(Orderable):
+    link_title = models.CharField(
+            verbose_name=_("titre"),
+            max_length=50,
+    )
+    link_page = models.ForeignKey(
+            "wagtailcore.Page",
+            related_name="+",
+            verbose_name=_("page"),
+            on_delete=models.CASCADE,
+    )
+    menu = ParentalKey(
+            "home.Menu",
+            related_name="menu_items",
+            on_delete=models.CASCADE,
+    )
+
+    panels = [
+            FieldPanel("link_title"),
+            PageChooserPanel("link_page"),
+    ]
+
+    @property
+    def title(self):
+        return self.link_title
+
+    @property
+    def link(self):
+        return self.link_page.url
+
+@register_snippet
+class Menu(ClusterableModel):
+    title = models.CharField(
+            verbose_name=_("titre"),
+            max_length=50,
+    )
+
+    panels = [
+            FieldPanel("title"),
+            InlinePanel("menu_items", label=_("éléments de menu")),
+    ]
+
+    def __str__(self):
+        return self.title
+
