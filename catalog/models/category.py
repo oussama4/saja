2323ef7c5196e6b3172a561b1d12c017d72e2076
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-02 22:13:26 +0100

    add categorie model

--- catalog/models/category.py
+++ catalog/models/category.py
@@ -1,4 +1,38 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Categorie(models.Model):
-    pass
+    name = models.CharField(verbose_name=_("Titre"), max_length=50)
+    slug = models.SlugField()
+    description = models.TextField(
+            verbose_name=_("Description"),
+            blank=True,
+            null=True,
+    )
+    image = models.ForeignKey(
+            "wagtailimages.Image",
+            blank=True,
+            null=True,
+            related_name="+",
+            on_delete=models.SET_NULL,
+    )
+    parent = models.ForeignKey(
+            "self",
+            blank=True,
+            null=True,
+            related_name="children",
+            on_delete=models.CASCADE,
+    )
+    brand = models.ForeignKey(
+            "catalog.Brand",
+            related_name="categories",
+            on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _("produit")
+        verbose_name_plural = _("produits")
+
+    def __str__(self):
+        return self.title
+
