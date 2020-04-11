f3e94767a56b9239b9f1f4eea75039e410422ba6
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-11 21:59:09 +0100

    solve password validation bug

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import password_validation
 
 from .models import User, Address
 
@@ -30,6 +31,15 @@ class UserCreationForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError(_("les mots de passe ne correspondent pas"))
         return password2
+
+    def _post_clean(self):
+        super()._post_clean()
+        password = self.cleaned_data.get("password2")
+        if password:
+            try:
+                password_validation.validate_password(password, self.instance)
+            except forms.ValidationError as error:
+                self.add_error("password2", error)
 
     def save(self, commit=True):
         # Save the provided password in hashed format
