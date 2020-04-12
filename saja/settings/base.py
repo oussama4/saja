b6c7a7250a0164b3fab40b1d8796c7b82fcff947
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-12 18:36:56 +0100

    add google social auth

--- saja/settings/base.py
+++ saja/settings/base.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     #'allauth.socialaccount.providers.facebook',
-    #'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.google',
 
     'django.contrib.admin',
     'django.contrib.auth',
