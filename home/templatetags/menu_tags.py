1070ce55c0b7987593448ba99b02b2ee8807db26
yassine <agamlmameyassine@gmail.com>
2020-04-14 04:04:18 +0100

    fix flip products and add some style

--- home/templatetags/menu_tags.py
+++ home/templatetags/menu_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+ 
 from ..models import Menu
 
 register = template.Library()
@@ -8,3 +8,7 @@ register = template.Library()
 def get_menu(title):
     return Menu.objects.get(title=title)
 
+
+@register.filter(name="split")
+def split(value):
+    return value.split('/')[1]
