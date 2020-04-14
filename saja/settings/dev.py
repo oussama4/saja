5c3a7a592ae2f247e5869d7befa9a37e179464f2
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-14 19:48:40 +0100

    add debug toolbar

--- saja/settings/dev.py
+++ saja/settings/dev.py
@@ -12,6 +12,13 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# debug_toolbar
+INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+MIDDLEWARE = MIDDLEWARE + [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+INTERNAL_IPS = ("127.0.0.1", )
+
 
 try:
     from .local import *
