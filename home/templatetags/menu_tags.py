9fa01c6e963d559b171e5be7b1bf868cd2175a71
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-17 20:13:05 +0100

    solve menu urls bug

--- home/templatetags/menu_tags.py
+++ home/templatetags/menu_tags.py
@@ -7,18 +7,23 @@ from ..models import Menu
 register = template.Library()
 
 
+def reshape_url(url):
+    l = url.split('/')[2:]
+    return '/' + '/'.join(l)
+
+
 @register.simple_tag()
 def catalog_menu():
     dump = Page.dump_bulk()
     brands = dump[0]['children'][0]['children']
     menu = []
     for b in brands:
-        d = {'title': b['data']['title'], 'url': b['data']['url_path'], 'children': []}
+        d = {'title': b['data']['title'], 'url': reshape_url(b['data']['url_path']), 'children': []}
         for c1 in b.get('children', []):
-            d2 = {'title': c1['data']['title'], 'url': c1['data']['url_path'], 'children': []}
+            d2 = {'title': c1['data']['title'], 'url': reshape_url(c1['data']['url_path']), 'children': []}
             for c2 in c1.get('children', []):
                 if c2['data']['show_in_menus']:
-                    d3 = {'title': c2['data']['title'], 'url': c2['data']['url_path'], 'children': []}
+                    d3 = {'title': c2['data']['title'], 'url': reshape_url(c2['data']['url_path']), 'children': []}
                     d2['children'].append(d3)
             d['children'].append(d2)
         menu.append(d)
