47b4fa5cae192a7f55abb1eac04c6ddb8db8b2f0
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-22 15:26:03 +0000

    redesign products

--- catalog/models/product.py
+++ catalog/models/product.py
@@ -2,23 +2,52 @@ import decimal
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils.text import slugify
 from django.core.validators import MaxValueValidator
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.models import ClusterableModel
 
+from wagtail.search import index
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.blocks import ListBlock, CharBlock
 from wagtail.snippets.models import register_snippet
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.admin.edit_handlers import (
         FieldPanel,
+        StreamFieldPanel,
         InlinePanel,
         MultiFieldPanel,
+        PageChooserPanel,
         TabbedInterface,
         ObjectList,
 )
+
+
+class ProductBase(models.Model):
+    """ abstract model for shared fields between product models and ranges """
+
+    title = models.CharField(verbose_name=_("Titre"), max_length=150)
+    slug = models.SlugField(unique=True)
+    description = RichTextField(
+            verbose_name=_("description"),
+            null=True,
+            blank=True,
+            features=["h2", "h4", "bold", "ol", "ul", "link"],
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    panels = [
+            FieldPanel("title"),
+            FieldPanel("description"),
+    ]
+
+    class Meta:
+        abstract = True
+
 
 @register_snippet
 class Attribute(models.Model):
@@ -37,6 +66,41 @@ class Attribute(models.Model):
     def __str__(self):
         return self.name
 
+@register_snippet
+class ProductRange(ProductBase):
+    brand = models.ForeignKey(
+            "catalog.Brand",
+            verbose_name=_("marque"),
+            related_name="ranges",
+            on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+            "catalog.Category",
+            verbose_name=_("categorie"),
+            related_name="ranges",
+            on_delete=models.CASCADE
+    )
+    range_image = models.ForeignKey(
+            "wagtailimages.Image",
+            verbose_name=_("image de gamme"),
+            related_name="+",
+            null=True,
+            blank=True,
+            on_delete=models.SET_NULL,
+    )
+
+    panels = ProductBase.panels + [
+            PageChooserPanel("brand", "catalog.Brand"),
+            PageChooserPanel("category", "catalog.Category"),
+            ImageChooserPanel("range_image"),
+    ]
+
+    class Meta:
+        verbose_name = _("Gamme")
+        verbose_name_plural = _("Gammes")
+
+    def __str__(self):
+        return self.title
 
 
 class ProductImages(Orderable):
@@ -57,16 +121,22 @@ class ProductImages(Orderable):
             ImageChooserPanel("product_image")
     ]
 
-class Product(Page):
-    subpage_types = ["catalog.Product"]
-
-    description = RichTextField(
-            verbose_name=_("description"),
-            null=True,
+class Product(ProductBase, index.Indexed, ClusterableModel):
+    features = StreamField(
+            [
+                ("features", ListBlock(CharBlock(min_length=5, max_length=150)))
+            ],
+            verbose_name=_("caractéristique"),
+            help_text=_("caractéristiques du produit"),
             blank=True,
-            features=["h2", "h4", "bold", "ol", "ul", "link"],
+            null=True
     )
-
+    product_range = models.ForeignKey(
+            ProductRange,
+            verbose_name=_("gamme"),
+            related_name="products",
+            on_delete=models.CASCADE
+    )
     base_price = models.DecimalField(
             verbose_name=_("prix"),
             max_digits=7,
@@ -82,11 +152,17 @@ class Product(Page):
             validators=[MaxValueValidator(90)]
     )
 
-    content_panels = Page.content_panels + [
-            FieldPanel("description"),
-            InlinePanel("product_images", label="images de produit"),
+    search_fields = [
+            index.SearchField("title", partial_match=True, boost=2)
     ]
 
+    panels = ProductBase.panels + [
+            StreamFieldPanel("features"),
+            SnippetChooserPanel("product_range")
+    ]
+    images_panels = [
+            InlinePanel("product_images", label="images de produit"),
+    ]
     price_panels = [
             MultiFieldPanel(
                 [
@@ -97,24 +173,34 @@ class Product(Page):
                 heading=_("informations sur les prix"),
             ),
     ]
-
     attribute_panels = [
             InlinePanel("attributes", label=_("attributs de produit"))
     ]
 
     edit_handler = TabbedInterface(
             [
-                ObjectList(content_panels, heading=_('Contenu')),
+                ObjectList(panels, heading=_('Contenu')),
+                ObjectList(images_panels, heading=_('Images')),
                 ObjectList(price_panels, heading=_('Prix')),
                 ObjectList(attribute_panels, heading=_("Attributs de produit")),
-                ObjectList(Page.promote_panels, heading=_('Promotion')),
-                ObjectList(Page.settings_panels, heading=_('Paramètres')),
             ]
     )
 
     class Meta:
         verbose_name = _("Produit")
         verbose_name_plural = _("Produits")
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        v = self.title
+        if self.attributes:
+            for attribute in self.attributes:
+                v += attribute.value
+        self.slug = slugify(v)
+        super(Product, self).save(*args, **kwargs)
 
     @property
     def price(self):
@@ -127,16 +213,6 @@ class Product(Page):
     @property
     def first_image(self):
         return self.product_images.first()
-
-    def get_context(self, request):
-        context = super().get_context(request)
-        prefetch = models.Prefetch(
-                lookup='product_images',
-                queryset=ProductImages.objects.select_related('product_image'),
-                to_attr='pimages'
-        )
-        context['product'] = Product.objects.prefetch_related(prefetch).live().public().get(pk=self.pk)
-        return context
 
 
 class AttributeProduct(Orderable):
--- checkout/models.py
+++ checkout/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-from django.shortcuts import redirect 
- 
-from catalog.models  import product 
-from users.models import User, Address 
-# Create your models here.
+from django.shortcuts import redirect
+
+from users.models import User, Address
 
 
 def total (items):
     total = 0
     for item in items:
         total+=item.total_price
-    return total 
+    return total
 
 
 class Cart(models.Model):
 
-    user_id = models.ForeignKey(User,related_name="+",on_delete=models.CASCADE)
+    user = models.ForeignKey(User,related_name="+",on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
-    
+
     @property
     def total_price(self):
         return total(self.items.all())
@@ -34,30 +32,32 @@ class Cart(models.Model):
         td = 0
         for item in self.items.all():
             td+=item.item_discount
-        return td 
-    
+        return td
+
     @property
     def price_without_discount(self):
-        return self.total_price+self.total_discount 
+        return self.total_price+self.total_discount
+
+
 class CartItem(models.Model):
 
-    cart_id = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
-    product_id = models.ForeignKey(product.Product ,related_name="+", on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey("catalog.Product" ,related_name="+", on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1,validators=[MaxValueValidator(10000)])
     created_date = models.DateTimeField(auto_now_add=True)
 
     @property
     def total_price(self):
-        return self.quantity*self.product_id.price
+        return self.quantity*self.product.price
     @property
     def item_discount(self):
         t =0
-        if self.product_id.has_discount:
-           t= (self.product_id.base_price*self.quantity)*self.product_id.discount_percent/100
+        if self.product.has_discount:
+           t= (self.product.base_price*self.quantity)*self.product.discount_percent/100
         return t
 
     def __str__(self):
-        return f"{self.quantity} x {self.product_id.title}"
+        return f"{self.quantity} x {self.product.title}"
 
 class Order(models.Model):
     Delivered = "D"
@@ -68,24 +68,18 @@ class Order(models.Model):
             (Padding, 'Padding'),
             (Refund, 'Refund')
         ]
-    user_id = models.ForeignKey(User, related_name="+", on_delete=models.PROTECT)
+    user = models.ForeignKey(User, related_name="+", on_delete=models.PROTECT)
     status = models.CharField(max_length=2,
-            choices=order_status, 
+            choices=order_status,
             default=Padding
         )
     payment_date = models.DateTimeField(auto_now_add=True)
     shipping_address = models.ForeignKey(Address, related_name="+",on_delete=models.PROTECT)
-    
-    #def save(self, *args, **kwargs):
-    #    if self.user_id.has_address:
-    #        super().save(*args, **kwargs)
-    #    else:
-    #        return False
 
-    @property 
+    @property
     def total_price(self):
         return total(self.items)
-    @property 
+    @property
     def delivered(self):
         self.status = self.Delivred
         self.save()
@@ -95,12 +89,12 @@ class Order(models.Model):
         self.save()
 
 class OrderItem(models.Model):
-    
-    order_id = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    product_id = models.ForeignKey(product.Product, related_name="+", on_delete=models.DO_NOTHING)
+
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey("catalog.Product", related_name="+", on_delete=models.DO_NOTHING)
     quantity = models.IntegerField(default=1, validators=[MaxValueValidator(10000)])
 
     @property
     def total_price(self):
-        return self.quantity * self.product_id.price
+        return self.quantity * self.product.price
 
