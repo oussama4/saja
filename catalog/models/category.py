c33ce277eb7cb2f2705c4d4b4d5fccbd6138fbe7
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-24 13:05:00 +0000

    add products listing ordering

--- catalog/models/category.py
+++ catalog/models/category.py
@@ -48,8 +48,8 @@ class Category(Page):
         )
 
     def get_context(self, request, *args, **kwargs):
-
             context = super().get_context(request, *args, **kwargs)
+
             descendant_categories = Category.objects.live().public().descendant_of(self)
             prefetch = Prefetch(
                 lookup='product_images',
@@ -59,10 +59,23 @@ class Category(Page):
             products_qs = Product.objects.select_related('product_range__category').prefetch_related(prefetch)
             products = products_qs.filter(product_range__category=self)
 
+            # if we're in a parent category
             subCat = []
             if descendant_categories:
                     subCat = descendant_categories
                     products = products_qs.filter(product_range__category__in=descendant_categories)
+
+            # ordering
+            order = request.GET.get("order", None)
+            if order:
+                if order == "title_asc":
+                    products = products.order_by('title')
+                elif order == "title_desc":
+                    products = products.order_by('-title')
+                elif order == "price_asc":
+                    products = products.order_by('base_price')
+                elif order == "price_desc":
+                    products = products.order_by('-base_price')
 
             #pagination
             paginator = Paginator(products, 3)
