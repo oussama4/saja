95ecde9acd819520439af9eaa3f46b922706e042
oussama zouaki <oussamazouaki4@gmail.com>
2020-04-26 15:03:20 +0000

    setup production settings

--- saja/settings/base.py
+++ saja/settings/base.py
@@ -195,7 +195,7 @@ WAGTAIL_SITE_NAME = "saja"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'sajacosmetics.com'
 
 AUTH_USER_MODEL = 'users.User'
 
--- saja/settings/production.py
+++ saja/settings/production.py
@@ -1,6 +1,26 @@
+import os
 from .base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = ['sajacosmetics.com', 'www.sajacosmetics.com']
+
+SECRET_KEY = os.environ['SECRET_KEY']
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+X_FRAME_OPTIONS = 'DENY'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# email
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = 'admin@sajacosmetics.com'
+EMAIL_HOST_USER = 'admin@sajacosmetics.com'
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 try:
     from .local import *
