60ff21e07b0a01375e602df00aea8a0d5fe9ae1b
osama zowaki <oussamazouaki4@gmail.com>
2020-04-27 20:01:22 +0000

    and finally email is implemented

--- saja/settings/production.py
+++ saja/settings/production.py
@@ -5,6 +5,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.sajacosmetics.com']
 
+INSTALLED_APPS = INSTALLED_APPS + ['anymail']
+
 SECRET_KEY = config.get('SECRET_KEY')
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
@@ -16,13 +18,21 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 # email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = 'admin@sajacosmetics.com'
-EMAIL_HOST_USER = 'admin@sajacosmetics.com'
-EMAIL_HOST_PASSWORD = 'Y5LBgSPcjACt'
+ANYMAIL = {
+    "MAILGUN_API_KEY": config.get('MAILGUN_API_KEY'),
+    "MAILGUN_SENDER_DOMAIN": 'mail.sajacosmetics.com',
+}
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = "osama@sajacosmetics.com"
+
+
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.zoho.com'
+#EMAIL_PORT = 465
+#EMAIL_USE_SSL = True
+#DEFAULT_FROM_EMAIL = 'admin@sajacosmetics.com'
+#EMAIL_HOST_USER = 'admin@sajacosmetics.com'
+#EMAIL_HOST_PASSWORD = 'Y5LBgSPcjACt'
 
 # logging
 LOGGING = {
