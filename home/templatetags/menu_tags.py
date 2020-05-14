e0da8e283d6b6fbdce7304c81edb037c465e4ddb
oussama zouaki <oussamazouaki4@gmail.com>
2020-05-14 18:29:25 +0000

    modify menu tag to not include pages other than categories

--- home/templatetags/menu_tags.py
+++ home/templatetags/menu_tags.py
@@ -20,12 +20,13 @@ def catalog_menu():
     for b in brands:
         d = {'title': b['data']['title'], 'url': reshape_url(b['data']['url_path']), 'children': []}
         for c1 in b.get('children', []):
-            d2 = {'title': c1['data']['title'], 'url': reshape_url(c1['data']['url_path']), 'children': []}
-            for c2 in c1.get('children', []):
-                if c2['data']['show_in_menus']:
-                    d3 = {'title': c2['data']['title'], 'url': reshape_url(c2['data']['url_path']), 'children': []}
-                    d2['children'].append(d3)
-            d['children'].append(d2)
+            if c1['data']['show_in_menus']:
+                d2 = {'title': c1['data']['title'], 'url': reshape_url(c1['data']['url_path']), 'children': []}
+                for c2 in c1.get('children', []):
+                    if c2['data']['show_in_menus']:
+                        d3 = {'title': c2['data']['title'], 'url': reshape_url(c2['data']['url_path']), 'children': []}
+                        d2['children'].append(d3)
+                d['children'].append(d2)
         menu.append(d)
 
     return menu
