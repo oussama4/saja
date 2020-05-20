ca2870198c2a5ad8bee4b330390413d6d7e2767e
oussama zouaki <oussamazouaki4@gmail.com>
2020-05-20 22:53:12 +0000

    exclude current product from variants

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,6 @@ def product(request, slug):
         to_attr='pimages'
     )
     p = get_object_or_404(Product.objects.select_related('product_range').prefetch_related(prefetch), slug=slug)
-    v = Product.objects.prefetch_related(prefetch).filter(product_range=p.product_range).all()
+    v = Product.objects.prefetch_related(prefetch).filter(product_range=p.product_range).exclude(pk=p.pk).all()
     return render(request, "catalog/product.html", {'product': p, 'variants': v})
 
