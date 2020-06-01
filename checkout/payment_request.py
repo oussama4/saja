06c5b7a76bd2e437afa592d2beeac97f65e6cf28
yassine <agamlmameyassine@gmail.com>
2020-06-01 09:20:02 +0100

    to merge

--- checkout/payment_request.py
+++ checkout/payment_request.py
@@ -11,8 +11,16 @@ def generateHash(attributes):
     h.update(toHash.encode())
 
     attributes['hash']=base64.b64encode(h.digest())
-
+    print(attributes['hash'].decode())
     return attributes
 
+
+d={
+        'amount':"44",
+        'clientid': "44445",
+        'billing': "154",
+}
+
+generateHash(d)
 
 
