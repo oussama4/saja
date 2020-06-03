d7ab7f528d0f3a27efb2458640cf65e396e7d2fa
yassine <agamlmameyassine@gmail.com>
2020-06-03 19:39:05 +0100

    postAuth almost finished

--- checkout/create_order.py
+++ checkout/create_order.py
@@ -7,7 +7,7 @@ def create_order(user):
     """ create an order from a cart model """
 
     try:
-        o = Order.objects.create(user=user, status=Order.PENDING, shipping_address=user.address)
+        o = Order.objects.create(user=user, status=Order.ONGOING, shipping_address=user.address)
         cart = user.carts.all()[0]
         for item in cart.items.all():
             order_item = OrderItem.objects.create(order=o, product=item.product, quantity=item.quantity)
--- checkout/views/post_auth_callback.py
+++ checkout/views/post_auth_callback.py
@@ -1,9 +1,33 @@
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse 
+import hashlib
+import html
+import base64 
+from ..payment_request import postAuth_gen_hash
+from ..models import Order 
+
 
 
 @csrf_exempt
 def host_to_host_callback(request):
-    print(request.POST)
-    return redirect('/')
+     
+    hash_dict = {k:html.unescape(v)  for k, v in request.POST.items() if k != "HASH" and k != "encoding"}
+     
+    try:
+        order = Order.objects.get(pk=request.POST['oid'])
+    except:
+        order = None
+
+    if(str(order.total_price) == request.POST['amount'] and postAuth_gen_hash(hash_dict, "Codylia2020") == base64.b64decode(request.POST['HASH'])):
+        if(str(request.POST['ProcReturnCode']) == '00'):
+            order.status = Order.PAID
+            order.save()
+            data = "ACTION=POSTAUTH"
+        else:
+            data = "APPROVED"
+    else:
+        data = "FAILURE"
+
+    return HttpResponse(data)
 
